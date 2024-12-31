72840ca38d392997bc16090ced26bfe70c597353
Arafat01007 <arafatfahadbro@gmail.com>
2024-12-31 15:38:36 +0600

    finished triangle and rectangle card

--- js/script.js
+++ js/script.js
@@ -8,6 +8,25 @@ function calTriangleArea() {
     const triangleHeightInput = document.getElementById('triangle-height');
     const triangleHeightValue = triangleHeightInput.value;
     const height = parseFloat(triangleHeightValue);
-    
 
+    const area = 0.5 * base * height;
+    console.log('Area of triangle is:', area);
+    // display triangle area
+    document.getElementById('triangle-area').innerText = area;
+
+
+}
+// Rectangle
+function calRecArea() {
+    // width
+    const rectangleWidthInput = document.getElementById('rectangle-width');
+    const width = parseFloat(rectangleWidthInput.value);
+    // height
+    const rectangleLengthInput = document.getElementById('rectangle-length');
+    const length = parseFloat(rectangleLengthInput.value);
+
+    const recArea = width*length;
+    console.log('Area of rectangle is:', recArea);
+  // display rectangle area
+  document.getElementById('rectangle-area').innerText = recArea;
 }

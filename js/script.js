57b067d16f98601e9aeac35c4df4e18cbfcb4fb2
Arafat01007 <arafatfahadbro@gmail.com>
2024-12-31 15:05:37 +0600

    added some function of triangle card

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 // Triangle 
-function calTriangleArea(){
-console.log('hello');
+function calTriangleArea() {
+    // base
+    const triangleBaseInput = document.getElementById('triangle-base');
+    const triangleBaseValue = triangleBaseInput.value;
+    const base = parseFloat(triangleBaseValue);
+    // height
+    const triangleHeightInput = document.getElementById('triangle-height');
+    const triangleHeightValue = triangleHeightInput.value;
+    const height = parseFloat(triangleHeightValue);
+    
+
 }
